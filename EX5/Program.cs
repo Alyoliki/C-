8494c8167571a4b6fba4937610ba33df1c97be7d
Alyona <alen4ik_@list.ru>
2023-03-12 02:18:41 +0600

    EX5

---
+++ EX5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя Пользователя, ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, Привет, Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
