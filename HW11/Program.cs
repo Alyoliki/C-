5fadb067cfbd0312bf5da4bfb2f47955b6e4e070
Alyona <alen4ik_@list.ru>
2023-03-23 20:27:35 +0600

    HW11

--- HW11/Program.cs
+++ HW11/Program.cs
@@ -1,1 +1,16 @@
-﻿// 
+﻿// Напишите цикл, который принимает на вход два 
+//числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Pow(n, b));
+
+
