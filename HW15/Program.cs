02077143f1959bfd28758529bce408e4b8015ca0
Alyona <alen4ik_@list.ru>
2023-03-26 23:46:49 +0600

    HW15

---
+++ HW15/Program.cs
@@ -0,0 +1,37 @@
+﻿// // Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10,90);
+}
+
+void ReleaseArray(int[] array)
+{
+    int s = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (i % 2 != 0) 
+            s = s + array[i];
+
+    Console.WriteLine ($" Сумма нечетных позиций = {s}");
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+Console.Clear();
+Console.WriteLine ($"n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+PrintArray(array);
+ReleaseArray(array);
