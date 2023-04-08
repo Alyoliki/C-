746dff6ed0bb361b3bb17e343faba1c30031269a
Alyona <alen4ik_@list.ru>
2023-04-08 11:50:22 +0600

    HW22

---
+++ HW22/Program.cs
@@ -0,0 +1,66 @@
+﻿//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+
+
+int[, ] InputMatrix()
+{
+    Console.Write("Введите размер матрицы: ");
+    int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+    int[, ] matrix = new int[size[0], size[1]];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 50);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+void ChangeElem(int [, ] matrix)
+{ 
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    int temp = matrix[i, j + 1];
+                    matrix[i, j + 1] = matrix[i, j];
+                    matrix[i, j] = temp;
+                }
+            
+        }
+        
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+Console.Clear();
+int[, ] matrix = InputMatrix();
+ChangeElem(matrix);
+Console.WriteLine();
+PrintArray(matrix);
