fd19e0a284d88305c073d13a46d73025baa81f5e
Alyona <alen4ik_@list.ru>
2023-04-09 12:52:01 +0600

    HW27

---
+++ HW27/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте значение N. Напишите программу,
+//  которая выведет все натуральные числа
+//   в промежутке от N до 1. Выполнить с 
+//   помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.Clear();
+
+String OutPutNumber(int Number)
+{
+    if(Number == 1)
+        return 1.ToString();
+    return Number.ToString() + ", " + OutPutNumber(Number - 1); 
+}
+
+Console.Write($" Введите натуральное число: ");
+int Number = int.Parse(Console.ReadLine());
+Console.Write(OutPutNumber(Number));
